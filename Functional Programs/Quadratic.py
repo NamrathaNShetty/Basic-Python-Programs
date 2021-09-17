07ea69df510dba2bdf54aa01855713b13e46c1f7
NamrathaNShetty <namratanshetty@gmail.com>
2021-09-17 21:09:18 +0530

    [Namratha] Add: To find the roots of the equation

---
+++ Functional Programs/Quadratic.py
@@ -0,0 +1,35 @@
+import math
+
+'''
+* @Author: Namratha N Shetty
+* @Date: 2021-09-12 15:28  
+* @Last Modified by: Namratha N Shetty
+* @Last Modified time: 2021-09-13 15:28
+* @Title: To find the roots of the equation
+'''
+
+
+def quadratic():
+    """
+    Description:
+        In this function the two roots of quadratic equation is found.
+        a, b, c are the three inputs to find the delta value.
+        The value of delta should be greater than 0 to have two real roots.
+    """ 
+    try:
+        a = int(input("Enter value for a : "))
+        b = int(input("Enter value for b : "))
+        c = int(input("Enter value for c : "))
+
+        delta = abs(b * b - 4 * a * c) # getting absolute value.
+
+        root1 = (-b + math.pow(delta, 1 / 2)) / (2 * a)
+        root2 = (-b - math.pow(delta, 1 / 2)) / (2 * a)
+        print("The First Root Of Equation Is " , root1)
+        print("The Second Root Of Equation Is ", root2)
+    
+    except ValueError:
+        print("Enter a Numeric value")
+        #quadratic()
+    
+quadratic()
